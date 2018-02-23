65ca8e6654338582fb487c743453762d23775aa4
Jordi Prats <jordi.prats@gmail.com>
2018-02-23 11:51:58 +0100

    lint

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,27 +1,27 @@
 class varnish::params {
 
-	case $::osfamily
-	{
-		'Debian':
-		{
-			case $::operatingsystem
-			{
-				'Ubuntu':
-				{
-					case $::operatingsystemrelease
-					{
-						/^14.*$/:
-						{
-              $sysconfig_file='/etc/default/varnish'
-              $sysconfig_template='varnish-debian-default.erb'
-						}
-						default: { fail("Unsupported Ubuntu version! - $::operatingsystemrelease")  }
-					}
-				}
-				'Debian': { fail("Unsupported")  }
-				default: { fail("Unsupported Debian flavour!")  }
-			}
-		}
-		default: { fail("Unsupported OS!")  }
-	}
+  case $::osfamily
+  {
+    'Debian':
+    {
+      case $::operatingsystem
+      {
+        'Ubuntu':
+        {
+          case $::operatingsystemrelease
+          {
+            /^14.*$/:
+            {
+                        $sysconfig_file='/etc/default/varnish'
+                        $sysconfig_template='varnish-debian-default.erb'
+            }
+            default: { fail("Unsupported Ubuntu version! - ${::operatingsystemrelease}")  }
+          }
+        }
+        'Debian': { fail('Unsupported')  }
+        default: { fail('Unsupported Debian flavour!')  }
+      }
+    }
+    default: { fail('Unsupported OS!')  }
+  }
 }
